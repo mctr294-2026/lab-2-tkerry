7793ef2d0555eca0501f7cc35904e9eed3afcdb4
Trent Kerry <tkerry@ualberta.ca>
2026-01-28 08:56:02 -0700

    root finding completed

---
+++ src/roots/roots.cpp
@@ -0,0 +1,102 @@
+#include "roots.hpp"
+#include <cmath>
+const double tolerance = 1e-6;
+
+bool bisection(std::function<double(double)> f,
+               double a, double b,
+               double *root)
+{    
+        
+        
+        if (f(a) * f(b) > 0) {
+            return false;
+        }
+            
+        for (int i = 0; i < 1e6; ++i) {
+            double mid = (a+b)/2;
+            
+            
+            if (abs(f(mid)) < tolerance){
+                *root = mid;
+                return true;
+            }
+                
+            if (f(a) * f(mid) < 0){
+                b = mid;
+                
+            } else{
+                a = mid;
+                
+            }
+
+            }
+
+            *root = (a + b)/2;
+            return true;
+        }
+
+bool regula_falsi(std::function<double(double)> f,
+                  double a, double b,
+                  double *root)
+{
+    
+    if (f(a) * f(b) > 0){
+        return false;
+    }
+    for ( int i = 0;  i < 1e6; i++){
+        double mid = a - (f(a)*(b - a))/(f(b) - f(a));
+        
+        if (abs(f(mid)) < tolerance){
+            *root = mid;
+        }
+
+        if (f(a) * f(mid) < 0){
+                b = mid;
+                
+            } else{
+                a = mid;
+                
+            } 
+
+    }
+    *root = a - (f(a)*(b - a))/(f(b) - f(a));
+    return true;
+}
+
+bool newton_raphson(std::function<double(double)> f,
+                    std::function<double(double)> g,
+                    double a, double b, double c,
+                    double *root)
+{
+    
+    for ( int i = 0;  i < 1e6; i++){
+        c -= f(c)/g(c);
+        
+        if (abs(f(c)/g(c)) < tolerance){
+            *root = c;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool secant(std::function<double(double)> f,
+            double a, double b, double c,
+            double *root)
+{
+    double d = c+1;
+    for ( int i = 0;  i < 1e6; i++){
+        d -= f(d)*(d-c)/(f(d)-f(c));
+
+        if (abs(d-c) < tolerance){
+            *root = d;
+            return true;
+        }
+
+
+    }
+    return false;
+}
+
+
